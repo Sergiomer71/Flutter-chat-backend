62309d3307b6a7fb868a2ca4dfd5ade5c275b48c
Sergio Merlo <sergiosamerlo@gmail.com>
2022-03-13 21:58:31 -0300

    Fin sección 10 - Backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,7 +85,7 @@ const login = async ( req, res = response ) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            ok: true,
+            ok: false,
             msg: 'Hable con el administrador'
         })
     }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,40 @@
 const { io } = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
+const { usuarioConectado, usuarioDesconectado, grabarMensaje } =  require('../controllers/socket');
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection',  (client) => {
     console.log('Cliente conectado');
 
+    // console.log( client.handshake.headers['x-token']);
+    const [ valido, uid]= comprobarJWT( client.handshake.headers['x-token'] )
+
+    // console.log(valido, uid);
+    // Verificar autenticación
+    if ( !valido ) { return client.disconnect();}
+
+    // console.log('cliente autenticado')
+    // Cliente autenticado
+    usuarioConectado( uid );
+
+    // INgresar al usuario a una sala en particular
+    // Sala global, 
+    // para enviar un mensaje privado lo hago en el client.id
+    client.join( uid );
+
+    // Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async (payload) => {
+        // console.log(payload);
+        // TODO: Grabar mensaje
+        await grabarMensaje( payload );
+        io.to( payload.para ).emit('mensaje-personal', payload);
+    })
+      
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        // console.log('Cliente desconectado');
+        usuarioDesconectado( uid );
     });
 
     
